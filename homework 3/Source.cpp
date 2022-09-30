37b9a925776905cf24aa14a811e7cddd3d0ea918
victo <victo@LAPTOP-2GBCPQMD>
2022-09-30 08:32:56 +0200

    Add project files.

---
+++ homework 3/Source.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<math.h>
+
+
+
+int main(void)
+{
+	int tal1;
+	int tal2;
+	int regne;
+
+	printf_s("%s", "enter first number: \n");
+	scanf_s("&d", &tal1);
+
+	
+
+	printf_s("%s", "enter second number: \n");
+	scanf_s("&d", &tal2);
+	
+
+	printf_s("%s", "enter operation: \n");
+	scanf_s("&d", &regne);
+
+
+
+	printf_s("%d", regne);
+	
+
+
+
+
+
+	return 0;
+}
